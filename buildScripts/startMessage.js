1dd8b3d9579ebee62b0da4ea8ea2eaa0aea31d21
Carolina Cordero Alvarez <ccalvarez@gmail>
2017-02-09 10:08:45 -0600

    Webpack and SourceMap added.

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,7 +1,7 @@
 //ES5:
-var chalk = require('chalk');
+//var chalk = require('chalk');
 
 //ES6, module syntax:
-//import chalk from 'chalk';
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting app in dev mode...'));
